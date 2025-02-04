aa4c6755ddcb39d2c01901e6f3bb90fbb38c3d39
SUENAGA Hiroki <hiroki_suenaga@mac.com>
2025-02-04 16:34:29 +0900

    enable module build.

--- devices/ssc338q_fpv_runcam-wifilinkjp/general/package/rtl8812au-aircrack/rtl8812au-aircrack.mk
+++ devices/ssc338q_fpv_runcam-wifilinkjp/general/package/rtl8812au-aircrack/rtl8812au-aircrack.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# rtl8812au-openipc
+# rtl8812au-aircrack
 #
 ################################################################################
 
@@ -10,7 +10,8 @@ RTL8812AU_AIRCRACK_VERSION = b44d288f423ede0fc7cdbf92d07a7772cd727de4
 RTL8812AU_AIRCRACK_LICENSE = GPL-2.0
 RTL8812AU_AIRCRACK_LICENSE_FILES = COPYING
 
-RTL8812AU_AIRCRACK_MODULE_MAKE_OPTS = CONFIG_RTL8812AU=m \
+RTL8812AU_AIRCRACK_MODULE_MAKE_OPTS = CONFIG_88XXAU=m \
+	CONFIG_80211W=n \
 	KVER=$(LINUX_VERSION_PROBED) \
 	KSRC=$(LINUX_DIR)
 
